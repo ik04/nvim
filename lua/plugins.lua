ed0eb627b7ab146f3ae39c3d3d82e87990d24e47
ik04 <ishaankhurana123@gmail.com>
2024-12-28 22:18:36 +0530

    chore: delete nvim-tree.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,2 +1,8 @@
-return {}
+return {
+    { "nvim-lualine/lualine.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+    { "navarasu/onedark.nvim" },
+    { "catppuccin/nvim", name = "catppuccin" },
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+}
 
