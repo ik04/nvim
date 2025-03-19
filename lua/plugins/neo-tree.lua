1ab319ebb513a99120cb34011e854a428b222cc4
ik04 <ishaankhurana123@gmail.com>
2025-03-19 17:48:15 +0530

    feat: add closing bind to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- Show hidden files
+                    hide_dotfiles = false, -- Don't hide dotfiles (e.g., .gitignore)
+                    hide_gitignored = false, -- Show files ignored by Git
+                }
+            }
+        })
         vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+        vim.keymap.set('n', '<C-q>', ':Neotree close<CR>', {}) -- Keybind to close Neo-tree
     end
 }
+
